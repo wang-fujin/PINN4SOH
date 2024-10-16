8ac20f34163e280b690bba021f8ab7ddbd543741
Fujin Wang <82586924+wang-fujin@users.noreply.github.com>
2024-10-16 09:07:30 +0800

    Update main_TJU.py

--- main_TJU.py
+++ main_TJU.py
@@ -36,7 +36,7 @@ def get_args():
 
     # loss related
     parser.add_argument('--alpha', type=float, default=1, help='loss = l_data + alpha * l_PDE + beta * l_physics')
-    parser.add_argument('--beta', type=float, default=50, help='loss = l_data + alpha * l_PDE + beta * l_physics')
+    parser.add_argument('--beta', type=float, default=0.05, help='loss = l_data + alpha * l_PDE + beta * l_physics')
 
     parser.add_argument('--log_dir', type=str, default='logging.txt', help='log dir, if None, do not save')
     parser.add_argument('--save_folder', type=str, default='results/TJU results', help='save folder')
