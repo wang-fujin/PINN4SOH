7515123895ea2c03d21fde3af12ec31fd88a8a14
Fujin Wang <82586924+wang-fujin@users.noreply.github.com>
2024-10-16 09:08:47 +0800

    Update main_HUST.py

--- main_HUST.py
+++ main_HUST.py
@@ -28,7 +28,7 @@ def get_args():
 
     # loss related
     parser.add_argument('--alpha', type=float, default=0.5, help='loss = l_data + alpha * l_PDE + beta * l_physics')
-    parser.add_argument('--beta', type=float, default=80, help='loss = l_data + alpha * l_PDE + beta * l_physics')
+    parser.add_argument('--beta', type=float, default=0.2, help='loss = l_data + alpha * l_PDE + beta * l_physics')
 
     parser.add_argument('--log_dir', type=str, default='logging.txt', help='log dir, if None, do not save')
     parser.add_argument('--save_folder', type=str, default='results/HUST results', help='save folder')
